b93ac725b6211e5f9ee54cb48c0c6d11578a60de
Jonas <jonas.rodriguez.external@eviden.com>
2025-05-30 12:27:14 +0200

    Allow white spaces on bid names

--- services/bidsService.js
+++ services/bidsService.js
@@ -6,14 +6,16 @@ module.exports = {
     const content = message.content.split(' ');
 
     // Check bid format
-    if (content.length !== 3) {      
+    if (content.length < 3) {
       logOnDiscord(null, message.client, `Formato de bid incorrecto en "${message.content}". Ejemplo, fulano robe 10.`);
       return;
     }
 
     const raider = content[0];
-    const item = content[1];
-    const dkp = content[2];
+    const dkp = content[content.length - 1];
+
+    // Merge all content except the first and last into the item
+    const item = content.slice(1, content.length - 1).join(' ');
 
     // Check if the dkp is a number
     if (!isNumeric(dkp)) {
